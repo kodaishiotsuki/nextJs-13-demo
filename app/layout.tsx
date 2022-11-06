7a21c70b17909a7fcc6316d8e4ec14937ef84c9a
kodaishiotsuki <0711dr5@gmail.com>
2022-11-06 19:41:32 +0900

    create SearchPage

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <head></head>
+      <head>
+        <title>Sample Next.js v13</title>
+      </head>
       <body>
         <Header />
         {children}
